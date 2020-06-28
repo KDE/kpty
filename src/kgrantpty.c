07fd95e73449e1e44b3bbe36ad6fe68101727e6b
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-06-28 11:21:39 +0200

    Convert license headers to SPDX statements

--- src/kgrantpty.c
+++ src/kgrantpty.c
@@ -19,23 +19,10 @@
  *
  * We have to chown/chmod the slave pty to prevent eavesdroping.
  *
- * Contributed by Zack Weinberg <zack@rabi.phys.columbia.edu>, 1998.
- * Copyright (c) 1999 by Lars Doelle <lars.doelle@on-line.de>.
+ * SPDX-FileCopyrightText: 1998 Zack Weinberg <zack@rabi.phys.columbia.edu>
+ * SPDX-FileCopyrightText: 1999 Lars Doelle <lars.doelle@on-line.de>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
 #include <config-pty.h>
