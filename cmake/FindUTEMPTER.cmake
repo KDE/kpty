6c5de35bb0f6f84388ff98d5d1fb37d8c3db5a4a
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-07-21 09:56:52 +0100

    Fix FindUTEMPTER when cross-compiling
    When cross-compiling we must not find utempter on the host system since
    that will result in an incorrect path being hardcoded into the library.
    Fix this by requiring UTEMPTER_EXECUTABLE to be set explicitly when
    cross-compiling. Alternatively you can set
    CMAKE_DISABLE_FIND_PACKAGE_UTEMPTER to skip UTEMPTER detection.
    
    This also removes the add_definitions() call from inside the find_package()
    call and replaces it with a new UTEMPTER_COMPILE_FLAGS variable since
    CMake find modules should not be changing compilation flags implicitly.

--- cmake/FindUTEMPTER.cmake
+++ cmake/FindUTEMPTER.cmake
@@ -9,35 +9,41 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-
-# use find_file instead of find_program until this is fixed:
-# https://gitlab.kitware.com/cmake/cmake/issues/10468
-find_file (UTEMPTER_EXECUTABLE utempter PATHS
-    ${KDE_INSTALL_FULL_LIBEXECDIR}/utempter
-    ${KDE_INSTALL_FULL_LIBDIR}/utempter
-    ${CMAKE_INSTALL_PREFIX}/libexec/utempter
-    ${CMAKE_INSTALL_PREFIX}/lib/utempter
-    /usr/libexec/utempter
-    /usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/utempter
-    /usr/lib/utempter
-)
-
-# On FreeBSD for example we have to use ulog-helper
-if (NOT UTEMPTER_EXECUTABLE)
-    find_program (UTEMPTER_EXECUTABLE ulog-helper PATHS
-        /usr/libexec
+if (CMAKE_CROSSCOMPILING)
+    # Avoid encoding a host path in the target library
+    if (NOT UTEMPTER_EXECUTABLE)
+        message(FATAL_ERROR "You must set UTEMPTER_EXECUTABLE or CMAKE_DISABLE_FIND_PACKAGE_UTEMPTER when cross-compiling")
+    endif()
+else ()
+    # use find_file instead of find_program until this is fixed:
+    # https://gitlab.kitware.com/cmake/cmake/issues/10468
+    find_file (UTEMPTER_EXECUTABLE utempter PATHS
+        ${KDE_INSTALL_FULL_LIBEXECDIR}/utempter
+        ${KDE_INSTALL_FULL_LIBDIR}/utempter
+        ${CMAKE_INSTALL_PREFIX}/libexec/utempter
+        ${CMAKE_INSTALL_PREFIX}/lib/utempter
+        /usr/libexec/utempter
+        /usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/utempter
+        /usr/lib/utempter
     )
-    if (UTEMPTER_EXECUTABLE)
-        add_definitions(-DUTEMPTER_ULOG=1)
+
+    # On FreeBSD for example we have to use ulog-helper
+    if (NOT UTEMPTER_EXECUTABLE)
+        find_program (UTEMPTER_EXECUTABLE ulog-helper PATHS /usr/libexec)
     endif ()
 endif ()
 
 if (UTEMPTER_EXECUTABLE)
-    add_definitions(-DUTEMPTER_PATH=\"${UTEMPTER_EXECUTABLE}\")
+    set(UTEMPTER_COMPILE_FLAGS -DUTEMPTER_PATH=\"${UTEMPTER_EXECUTABLE}\")
+    get_filename_component(UTEMPTER_EXECUTABLE_NAME "${UTEMPTER_EXECUTABLE}" NAME)
+    if ("${UTEMPTER_EXECUTABLE_NAME}" STREQUAL ulog-helper)
+        message(STATUS "UTEMPTER: using ulog-helper")
+        set(UTEMPTER_COMPILE_FLAGS ${UTEMPTER_COMPILE_FLAGS} -DUTEMPTER_ULOG=1)
+    endif ()
 endif ()
 
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (UTEMPTER DEFAULT_MSG UTEMPTER_EXECUTABLE)
+find_package_handle_standard_args (UTEMPTER REQUIRED_VARS UTEMPTER_EXECUTABLE UTEMPTER_COMPILE_FLAGS)
 
 
 set_package_properties (UTEMPTER PROPERTIES
