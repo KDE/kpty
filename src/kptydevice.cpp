7b38a16cb618dc0bb9669294e74d598b957fa3dc
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2024-06-27 15:17:55 +0000

    Apply 2 suggestion(s) to 1 file(s)

--- src/kptydevice.cpp
+++ src/kptydevice.cpp
@@ -432,10 +432,10 @@ void KPtyDevicePrivate::finishOpen(QIODevice::OpenMode mode)
     readBuffer.clear();
     readNotifier = new QSocketNotifier(q->masterFd(), QSocketNotifier::Read, q);
     writeNotifier = new QSocketNotifier(q->masterFd(), QSocketNotifier::Write, q);
-    QObject::connect(readNotifier, &QSocketNotifier::activated, [this]() {
+    QObject::connect(readNotifier, &QSocketNotifier::activated, q, [this]() {
         _k_canRead();
     });
-    QObject::connect(writeNotifier, &QSocketNotifier::activated, [this]() {
+    QObject::connect(writeNotifier, &QSocketNotifier::activated, q, [this]() {
         _k_canWrite();
     });
     readNotifier->setEnabled(true);
